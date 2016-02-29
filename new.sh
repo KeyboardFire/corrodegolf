eb3b8854d7c8e6921c950a816c26a8ffb6b581c1
KeyboardFire <andy@keyboardfire.com>
2016-02-29 13:34:04 -0600

    Add helper script for creating templates

---
+++ new.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]
+then
+    echo 'incorrect number of arguments'
+    echo "usage: $0 QUESTIONID"
+    exit 1
+fi
+
+if [[ ! "$1" =~ ^[0-9]{1,6}$ ]]
+then
+    echo 'invalid input format'
+    echo "usage: $0 QUESTIONID"
+    exit 1
+fi
+
+id="$(printf %06d $1)"
+filename="src/q${id}.rs"
+
+if [ -f $filename ]
+then
+    echo "file $filename already exists"
+    exit 1
+fi
+
+cat <<EOF > $filename
+pub fn q$id() {
+}
+
+#[test]
+fn test$id() {
+}
+EOF
+
+cat <<EOF >> src/lib.rs
+
+// http://codegolf.stackexchange.com/q/$1/3808
+mod q$id;
+pub use q$id::q$id;
+EOF
